453479c749b4cb46cc8a4feaabd8818ef7478bc1
Andrew <andrew@andrewsmith.org>
2022-12-05 23:48:50 -0500

    Add basic testing for using state

--- src/App.js
+++ src/App.js
@@ -1,54 +1,16 @@
+import { useState } from "react";
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+  const [counter, setCounter] = useState(0);
 
+  const handleClick = () => {
+    console.log("clicked");
+  };
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <div>{counter}</div>
+      <button onClick={handleClick}> plus </button>{" "}
     </div>
   );
 };
-
-const Header = (props) => <h1>{props.course}</h1>;
-
-const Content = (props) => (
-  <>
-    <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-    <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-    <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-  </>
-);
-
-const Part = (props) => (
-  <p>
-    {props.part} {props.exercises}
-  </p>
-);
-
-const Total = (props) => (
-  <p>
-    Number of exercises{" "}
-    {props.parts[0].exercises +
-      props.parts[1].exercises +
-      props.parts[2].exercises}
-  </p>
-);
 
 export default App;
