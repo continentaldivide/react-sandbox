7673287b372f4046a83ff5cb643ecece115071c6
Andrew <andrew@andrewsmith.org>
2022-12-10 23:02:53 -0500

    Practice and experiment working with state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { useState } from "react";
 const App = () => {
   const [counter, setCounter] = useState(0);
+  const increaseByOne = () => setCounter(counter + 1);
+  const setToZero = () => setCounter(0);
 
-  const handleClick = () => {
-    console.log("clicked");
-  };
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={handleClick}> plus </button>{" "}
+      <div>
+        <Display counter={counter} />
+      </div>
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
     </div>
   );
 };
+
+const Display = ({ counter }) => <div>{counter}</div>;
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 export default App;
